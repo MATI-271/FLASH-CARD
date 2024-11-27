86a18095935bb7c368193ab970da7cf74b58ce36
MATI-271 <matihbp@gmail.com>
2024-11-26 23:51:18 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente em área territorial do mundo?',
+    'A Ásia, com cerca de 44,5 milhões de Km³'
+)
+
+criaCartao(
+    'História',
+    'Qual é a data da Independência do Brasil?',
+    '7 de setembro de 1822'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotosíntese?',
+    'A conversão da luz solar em energia pelas plantas'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é o conceito de porcentagem?',
+    'Porcentagem é uma fração que representa uma parte de 100% (ex: 25% = 25/100)'
+)
